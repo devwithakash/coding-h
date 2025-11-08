616944bf4cb9846cc166b9890e486f8a23f534aa
Akash <hackerrank.akash@gmail.com>
2025-11-08 11:57:34 +0000

    client

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,22 +3,24 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    // This is the proxy configuration
-    proxy: {
-      // Any request starting with /api will be proxied
-      '/api': {
-        target: 'http://localhost:8080', // Your Spring Boot backend
-        changeOrigin: true,
-        secure: false,
-      },
-      // Also proxy the OAuth2 login path
-      '/oauth2': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-      }
+    plugins: [react()],
+    base: '/coding-h/client/',
+    publicDir: 'public',
+    server: {
+      // This is the proxy configuration
+      proxy: {
+        // Any request starting with /api will be proxied
+        '/api': {
+          target: 'https://coding-h.onrender.com', 
+          changeOrigin: true,
+          secure: false,
+        },
+        // Also proxy the OAuth2 login path
+        '/oauth2': {
+          target: 'https://coding-h.onrender.com',
+          changeOrigin: true,
+          secure: false,
+        }
     }
   }
 })
